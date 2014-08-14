8998141ba510baf5c8390435fa0ded9ee4310cca
Balázs Grill <balage.g@gmail.com>
2014-08-14 16:00:20 +0200

    Added test suite

---
+++ hu.temon.expressions.test/src/hu/temon/expressions/test/ExpressionTestsSuite.java
@@ -0,0 +1,11 @@
+package hu.temon.expressions.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ExpressionTests.class })
+public class ExpressionTestsSuite {
+
+}
